34d78f7bff73ffe61ddc0e00cebbea21db800d0d
kdave007 <kevin.maldonado.dev@gmail.com>
2025-01-22 21:35:06 -0600

    restoring changes lost, dialog and outcome table

--- src/app/income-outcome/outcome-table/outcome-dialog/outcome-dialog.component.ts
+++ src/app/income-outcome/outcome-table/outcome-dialog/outcome-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import moment from 'moment';
 
 @Component({
   selector: 'app-outcome-dialog',
@@ -6,5 +8,63 @@ import { Component } from '@angular/core';
   styleUrl: './outcome-dialog.component.css'
 })
 export class OutcomeDialogComponent {
+  amount = { integer: 0, decimal: 0 };
 
+  constructor(
+    public dialogRef: MatDialogRef<OutcomeDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    if (this.data.date) {
+      this.data.date = this.convertToDate(this.data.date);
+    }
+
+    if (this.data.amount == null || this.data.amount == undefined) {
+      this.data.amount = 0.0;
+    }
+
+    this.amount = this.splitAmount(this.data.amount);
+  }
+
+  convertToDate(dateString: string): Date {
+    return moment(dateString, 'YYYY-MM-DD').toDate();
+  }
+
+  convertToApiFormat(date: Date): string {
+    return moment(date).format('YYYY-MM-DD');
+  }
+
+  splitAmount(amount: number) {
+    const amountParts = amount.toFixed(2).split('.');
+    return {
+      integer: parseInt(amountParts[0], 10),
+      decimal: parseInt(amountParts[1], 10)
+    };
+  }
+
+  combineParts(integerPart: any, decimalPart: any): number {
+    if (integerPart == undefined && decimalPart == undefined) {
+      return parseFloat(`${0}.${0}`);
+    }
+    const decimalValue = decimalPart ? decimalPart.toString().padStart(2, '0') : '00';
+    return parseFloat(`${integerPart}.${decimalValue}`);
+  }
+
+  limitNumberLength(event: any, maxLength: number) {
+    let value = event.target.value;
+    value = value.replace(/[^0-9]/g, '');
+    if (value.length > maxLength) {
+      value = value.slice(0, maxLength);
+    }
+    event.target.value = value;
+  }
+
+  save(): void {
+    this.data.amount = this.combineParts(this.amount.integer, this.amount.decimal);
+    this.data.date = this.convertToApiFormat(this.data.date);
+    this.dialogRef.close(this.data);
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/income-outcome/outcome-table/outcome-table.component.ts
+++ src/app/income-outcome/outcome-table/outcome-table.component.ts
@@ -1,11 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, NgZone } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { SelectionModel } from '@angular/cdk/collections';
 import { MatDialog } from '@angular/material/dialog';
+import { OutcomeDialogComponent } from './outcome-dialog/outcome-dialog.component';
+
+export interface OutcomeElement {
+  input_id: number;
+  date: string;
+  amount: number;
+  form: 'credit' | 'cash';
+  concept: string;
+  description: string;
+  isEditing?: boolean;
+  amountIntegerPart?: number;
+  amountDecimalPart?: number;
+  lastValues?: object;
+}
 
 @Component({
   selector: 'app-outcome-table',
   templateUrl: './outcome-table.component.html',
   styleUrl: './outcome-table.component.css'
 })
-export class OutcomeTableComponent {
-  constructor(public dialog: MatDialog) {}
+export class OutcomeTableComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['select', 'input_id', 'date', 'form', 'amount', 'concept', 'description', 'actions'];
+  
+  selection = new SelectionModel<OutcomeElement>(true, []);
+
+  maxLengthInputValues = {
+    concept: 25,
+    description: 25,
+    amount: 10
+  }
+
+  ELEMENT_DATA: OutcomeElement[] = [
+    { input_id: 1, date: '2024-08-01', amount: 18.99, form: 'credit', concept: 'Rent', description: 'Monthly rent payment' },
+    { input_id: 2, date: '2024-08-02', amount: 45.25, form: 'cash', concept: 'Utilities', description: 'Electricity bill' },
+    { input_id: 3, date: '2024-08-03', amount: 12.90, form: 'credit', concept: 'Internet', description: 'Monthly internet service' },
+    { input_id: 4, date: '2024-08-04', amount: 30.00, form: 'cash', concept: 'Gas', description: 'Car fuel' },
+    { input_id: 5, date: '2024-08-05', amount: 22.13, form: 'credit', concept: 'Insurance', description: 'Car insurance payment' }
+  ];
+
+  dataSource = new MatTableDataSource<OutcomeElement>(this.ELEMENT_DATA);
+
+  // Column Filter
+  filterColumn: string = 'concept'; // Default to concept filter
+  filterValue: string = '';
+
+  constructor(private zone: NgZone, public dialog: MatDialog) {}
+
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  openOutcomeDialog(record: any): void {
+    // TODO: Implement OutcomeDialogComponent
+    const dialogRef = this.dialog.open(OutcomeDialogComponent, {
+      width: '400px',
+      data: { ...record },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        for (const [key, value] of Object.entries(result)) {
+          record[key] = result[key];
+        }
+      }
+    });
+  }
+
+  applyFilter() {
+    const filterValue = this.filterValue.trim().toLowerCase();
+
+    this.dataSource.filterPredicate = (data: OutcomeElement, filter: string) => {
+      if (this.filterColumn === 'input_id') {
+        return data.input_id.toString().toLowerCase().includes(filter);
+      } else if (this.filterColumn === 'date') {
+        return data.date.toLowerCase().includes(filter);
+      } else if (this.filterColumn === 'concept') {
+        return data.concept.toLowerCase().includes(filter);
+      }
+      return false;
+    };
+
+    this.dataSource.filter = filterValue;
+  }
 }
